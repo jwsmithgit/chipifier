b56d51cb359627f326fbfe61ed2b9f17b6b9aff4
Jacob Smith <jacobwillsmith@gmail.com>
2016-03-06 15:22:07 -0800

    couple classes
    couple classes

---
+++ composition.py
@@ -0,0 +1,34 @@
+import note
+
+class Composition {
+    def __init__( self ) :
+		self.tempo = -1
+        self.notes = [ ]
+
+    def __init__( self, notes ) :
+        self.notes += notes
+
+	# add notes to composition
+    def add_note( self, note ) :
+        self.notes += note
+
+    # add notes to composition
+    def add_notes( self, notes ) :
+		self.notes += notes
+
+    # remove a note from the composition
+    def remove_note( self, note ) :
+        self.notes.remove( note )
+
+    # prints a pretty composition
+    def __str__( self ) :
+        string = '['
+        for note in self.notes :
+			print(note)
+		string += ']'
+        return string
+
+	# sort by start times
+    def sort_by_start_times( self ) :
+		notes = sorted(self.notes, key = lambda note: int(note[0]))
+}
---
+++ note.py
@@ -0,0 +1,49 @@
+class Note {
+	# CONSTRUCTORS
+	def __init__( self ) :
+        self.start_time = -1
+		self.end_time = -1
+		self.frequency = -1
+		self.amplitude = -1
+
+    def __init__( self, start_time, end_time, frequency, amplitude ) :
+        self.start_time = start_time
+		self.end_time = end_time
+		self.frequency = frequency
+		self.amplitude = amplitude
+
+	# SETTERS
+    def set_start_time( self, start_time ) :
+        self.start_time = start_time
+
+	def set_end_time( self, end_time ) :
+		self.end_time = end_time
+
+	def set_frequency( self, frequency ) :
+		self.frequency = frequency
+
+	def set_amplitude( self, amplitude ) :
+		self.amplitude = amplitude
+
+	# GETTERS
+	def get_start_time( self ) :
+		return self.start_time
+
+	def get_end_time( self ) :
+		return self.end_time
+
+	def get_frequency( self ) :
+		return self.frequency
+
+	def get_amplitude( self ) :
+		return self.amplitude
+
+	# PRINT
+    def __str__( self ) :
+        string = '('
+        string += 'start time: ' + str(self.get_start_time())
+        string += ', end time: ' + str(self.get_end_time())
+        string += ', frequency: ' + str(self.get_frequency())
+        string += ', amplitude: ' + str(self.get_amplitude())
+		string += ')'
+        return string

bf3217ff6d6575903f1d98e4101651dec884bc7d
Jacob Smith <jacobwillsmith@gmail.com>
2016-03-06 14:53:58 -0800

    Real changers
    changers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import transriber.py
-import nesmidi.py
-import generatesound.py
+import sonic_scanner.py
+import retro_conformer.py
+import wave_generator.py
 
 if __name__ == "__main__" :
     print("MAIN")

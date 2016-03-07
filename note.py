569807d5dcbb5287a40f7053647bd1057d310b9a
Jacob Smith <jacobwillsmith@gmail.com>
2016-03-06 17:55:55 -0800

    spaces

--- note.py
+++ note.py
@@ -4,45 +4,45 @@ a note is a sound that is made for a duration of time at a certain frequency
 '''
 
 class Note :
-	# CONSTRUCTOR
-	def __init__( self, start_time=-1, end_time=-1, frequency=-1, amplitude=-1 ) :
-		self.start_time = start_time
-		self.end_time = end_time
-		self.frequency = frequency
-		self.amplitude = amplitude
+    # CONSTRUCTOR
+    def __init__( self, start_time=-1, end_time=-1, frequency=-1, amplitude=-1 ) :
+        self.start_time = start_time
+        self.end_time = end_time
+        self.frequency = frequency
+        self.amplitude = amplitude
 
-	# SETTERS
-	def set_start_time( self, start_time ) :
-		self.start_time = start_time
+    # SETTERS
+    def set_start_time( self, start_time ) :
+        self.start_time = start_time
 
-	def set_end_time( self, end_time ) :
-		self.end_time = end_time
+    def set_end_time( self, end_time ) :
+        self.end_time = end_time
 
-	def set_frequency( self, frequency ) :
-		self.frequency = frequency
+    def set_frequency( self, frequency ) :
+        self.frequency = frequency
 
-	def set_amplitude( self, amplitude ) :
-		self.amplitude = amplitude
+    def set_amplitude( self, amplitude ) :
+        self.amplitude = amplitude
 
-	# GETTERS
-	def get_start_time( self ) :
-		return self.start_time
+    # GETTERS
+    def get_start_time( self ) :
+        return self.start_time
 
-	def get_end_time( self ) :
-		return self.end_time
+    def get_end_time( self ) :
+        return self.end_time
 
-	def get_frequency( self ) :
-		return self.frequency
+    def get_frequency( self ) :
+        return self.frequency
 
-	def get_amplitude( self ) :
-		return self.amplitude
+    def get_amplitude( self ) :
+        return self.amplitude
 
-	# PRINT
-	def __str__( self ) :
-		string = '('
-		string += 'start time: ' + str(self.get_start_time())
-		string += ', end time: ' + str(self.get_end_time())
-		string += ', frequency: ' + str(self.get_frequency())
-		string += ', amplitude: ' + str(self.get_amplitude())
-		string += ')'
-		return string
+    # PRINT
+    def __str__( self ) :
+        string = '('
+        string += 'start time: ' + str(self.get_start_time())
+        string += ', end time: ' + str(self.get_end_time())
+        string += ', frequency: ' + str(self.get_frequency())
+        string += ', amplitude: ' + str(self.get_amplitude())
+        string += ')'
+        return string
--- wave_generator.py
+++ wave_generator.py
@@ -6,55 +6,55 @@ import composition
 import wave
 
 def square_wave( length, amplitude, frequency ) :
-	section = frequency // 2
-	x = np.linspace(amplitude, amplitude, section)
-	mx = -x
+    section = frequency // 2
+    x = np.linspace(amplitude, amplitude, section)
+    mx = -x
 
-	t = np.concatenate( (x, mx) )
-	while len(t) < length :
-		t = np.concatenate( (t, t) )
-	t = t[:length]
+    t = np.concatenate( (x, mx) )
+    while len(t) < length :
+        t = np.concatenate( (t, t) )
+    t = t[:length]
 
-	return t
+    return t
 
 def triangle_wave( length, amplitude, frequency ) :
-	section = frequency // 4
-	x = np.linspace(0, amplitude, section+1)
-	mx = -x
+    section = frequency // 4
+    x = np.linspace(0, amplitude, section+1)
+    mx = -x
 
-	t = np.concatenate( (x, x[-2:0:-1], mx[1:], mx[-2:0:-1]) )
-	while len(t) < length :
-		t = np.concatenate( (t, t) )
-	t = t[:length]
+    t = np.concatenate( (x, x[-2:0:-1], mx[1:], mx[-2:0:-1]) )
+    while len(t) < length :
+        t = np.concatenate( (t, t) )
+    t = t[:length]
 
-	return t
+    return t
 
 def noise_wave( length, amplitude ) :
-	t = np.random.rand(length)
-	t = np.multiply(t,amplitude)
+    t = np.random.rand(length)
+    t = np.multiply(t,amplitude)
 
-	return t
+    return t
 
 def mix_waves( waves ):
-	mix = np.zeros[len(waves[0])]
-	for wave in waves :
-		mix = [x + y for x, y in zip( mix, wave )]
+    mix = np.zeros[len(waves[0])]
+    for wave in waves :
+        mix = [x + y for x, y in zip( mix, wave )]
 
-	return mix
+    return mix
 
 def write_to_file( data, filename, params ):
-	f = wave.open( filename,'w' )
-	f.setparams( params )
-	f.writeframes( data.astype(np.int16).tostring() )
+    f = wave.open( filename,'w' )
+    f.setparams( params )
+    f.writeframes( data.astype(np.int16).tostring() )
 
 if __name__ == "__main__" :
-	s_wave = square_wave(880,1,440)
-	t_wave = triangle_wave(800,1,440)
-	n_wave = noise_wave( 800, 1 )
+    s_wave = square_wave(880,1,440)
+    t_wave = triangle_wave(800,1,440)
+    n_wave = noise_wave( 800, 1 )
 
-	'''plot(n_wave)
-	show()
-	print(len(n_wave))
-	print("GENERATESOUND")'''
+    '''plot(n_wave)
+    show()
+    print(len(n_wave))
+    print("GENERATESOUND")'''
 
-	comp = composition.Composition()
+    comp = composition.Composition()

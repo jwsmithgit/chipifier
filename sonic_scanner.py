466e6308b5ec6a1393350dac556ff5e98880bd9a
Jacob Smith <jacobwillsmith@gmail.com>
2016-03-06 23:58:38 -0800

    comments

--- sonic_scanner.py
+++ sonic_scanner.py
@@ -1,71 +1,51 @@
-import numpy as np
 import wave
+import cmath
+
+import numpy as np
 from scipy.fftpack import fft,rfft
 from scipy.io.wavfile import read
 from scipy import signal
+
 import composition
 import note
-import matplotlib.pyplot as ml
-import cmath
 
 BUFFER_SIZE = 1024
 SEGMENT_SIZE = 1024
 
-def create_note(freq, seg_num, ampl):
+def create_note( freq, seg_num, ampl ):
     start_time = seg_num * SEGMENT_SIZE
     end_time = start_time + SEGMENT_SIZE-1
-    return note.Note(start_time, end_time, freq, ampl)
+    return note.Note( start_time, end_time, freq, ampl )
 
-#COPIED REMOVE
-def parabolic(f, x):
-    """Quadratic interpolation for estimating the true position of an
-    inter-sample maximum when nearby samples are known.
-
-    f is a vector and x is an index for that vector.
-
-    Returns (vx, vy), the coordinates of the vertex of a parabola that goes
-    through point x and its two neighbors.
-
-    Example:
-    Defining a vector f with a local maximum at index 3 (= 6), find local
-    maximum if points 2, 3, and 4 actually defined a parabola.
-
-    In [3]: f = [2, 3, 1, 6, 4, 2, 3, 1]
-
-    In [4]: parabolic(f, argmax(f))
-    Out[4]: (3.2142857142857144, 6.1607142857142856)
-
-    """
-    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
-    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
-    return (xv, yv)
-
-# copy of main right now
 def scan() :
-    print("Freq detection")
-    wave_ifile = wave.open('sound1.wav', 'r')
+    print( "Freq detection" )
+    wave_ifile = wave.open( 'sound1.wav', 'r' )
     frame_rate = wave_ifile.getframerate()
     comp = composition.Composition()
-    segment_num=0
+    segment_num = 0
 
     while True:
-        iframes = wave_ifile.readframes(BUFFER_SIZE)
+        iframes = wave_ifile.readframes( BUFFER_SIZE )
         if not iframes:
             break
 
-        data = np.fromstring(iframes, np.int16)
-        windowed = data * signal.hamming(len(data))
+        data = np.fromstring( iframes, np.int16 )
+        windowed = data * signal.hamming( len(data) )
 
-        fft_data = fft(windowed)
-        bin_number = np.argmax(abs(fft_data))
-        #bin_number = parabolic(np.log(abs(fft_data)), bin_number)[0]
+        fft_data = fft( windowed )
+        bin_number = np.argmax( abs(fft_data) )
 
-        complex_coordinate = fft_data[bin_number] /SEGMENT_SIZE*10
-        amplitude = cmath.polar(complex_coordinate)[0]
+        freq = bin_number * ( frame_rate / SEGMENT_SIZE ) 
+	# need to crush to musical notes
+	# A0 is 27.5 all steps are multiplied by 12root(2) or 1.0594 steps up until A9 14080
+	# see monophonic pitch slide 24
 
-        freq = bin_number /SEGMENT_SIZE *frame_rate
-        comp.add_note( create_note(freq, segment_num, amplitude) )
-        segment_num+=1
+	# the amplitude is the radius retrieved form real number and imaginary number
+        complex_coordinate = fft_data[bin_number] # * ( 10 / SEGMENT_SIZE )
+        amplitude = cmath.polar( complex_coordinate )[0]
+
+        comp.add_note( create_note( freq, segment_num, amplitude ) )
+        segment_num += 1
 
     wave_ifile.close()
 

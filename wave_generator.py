edf2277c09a328f7052a8f8c1943c587e360f1e0
Jacob Smith <jacobwillsmith@gmail.com>
2016-03-06 19:33:25 -0800

    wave changes

--- wave_generator.py
+++ wave_generator.py
@@ -4,6 +4,7 @@ from pylab import show
 from scipy import signal
 import composition
 import wave
+import math
 
 def square_wave( length, amplitude, frequency ) :
     section = frequency // 2
@@ -38,8 +39,7 @@ def noise_wave( length, amplitude ) :
 def mix_waves( waves ):
     mix = np.zeros[len(waves[0])]
     for wave in waves :
-        mix = [x + y for x, y in zip( mix, wave )]
-
+        mix = np.add(mix,wave)
     return mix
 
 def write_to_file( data, filename, params ):
@@ -47,14 +47,32 @@ def write_to_file( data, filename, params ):
     f.setparams( params )
     f.writeframes( data.astype(np.int16).tostring() )
 
-if __name__ == "__main__" :
-    s_wave = square_wave(880,1,440)
-    t_wave = triangle_wave(800,1,440)
-    n_wave = noise_wave( 800, 1 )
+def generate( composition ) :
+    notes = composition.notes
+    s_wave = np.array([])
+    for note in notes :
+        length = note.end_time - note.start_time
+        s_wave = np.concatenate( (s_wave, square_wave(length, 10000, 1/(note.frequency/44100))) )
+    params = (1, 2, 44100, len(n_wave), 'NONE', 'not compressed')
+    write_to_file(s_wave, 'square.wav', params)
 
+
+
+if __name__ == "__main__" :
+    s_wave = np.array([])
+    t_wave = np.array([])
+    i = 100
+    for i in range(100,4000,10):
+        s_wave = np.concatenate( (s_wave, square_wave(44100/100, 10000, 1/(i/44100))) )
+        t_wave = np.concatenate( (t_wave, triangle_wave(44100/100, 10000, 1/(i/44100))) )
+
+    n_wave = noise_wave( 44100*10, 10000 )
+
+    params = (1, 2, 44100, len(n_wave), 'NONE', 'not compressed')
+    write_to_file(s_wave, 'square.wav', params)
+    write_to_file(t_wave, 'triangle.wav', params)
+    write_to_file(n_wave, 'noise.wav', params)
     '''plot(n_wave)
     show()
     print(len(n_wave))
     print("GENERATESOUND")'''
-
-    comp = composition.Composition()
